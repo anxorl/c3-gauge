5e48178c13f7f8613f7396f672c8eeb89925ebd3
Anxo <anxo@anxorl.es>
2018-02-19 19:17:25 +0100

    Actualizacion de c3 Eliminada a lenda que agora ponse automáticamente.

--- src/c3-gauge/c3-gauge.module.ts
+++ src/c3-gauge/c3-gauge.module.ts
@@ -5,67 +5,70 @@ import * as c3 from 'c3';
 import 'c3/c3.min.css'
 
 @Directive({
-  selector: 'c3-gauge'
+    selector: 'c3-gauge'
 })
 export class GaugeDirective implements OnChanges, OnInit {
 
-  @Input() public data: any;
-  @Input() public chartOptions: any;
-  @Input() public configs: c3.ChartConfiguration;
-  @Input() public type: any;
+    @Input() public data: any;
+    @Input() public chartOptions: any;
+    @Input() public configs: c3.ChartConfiguration;
+    @Input() public type: any;
 
-  private element: ElementRef;
-  private chart: c3.ChartAPI;
-  private _f: (n: number) => string = (n: number) => { return n < 100 ? d3.format('.1f')(n) : d3.format('.0f')(n) };
+    private element: ElementRef;
+    private chart: c3.ChartAPI;
+    private _f: (n: number) => string = (n: number) => { return n < 100 ? d3.format('.1f')(n) : d3.format('.0f')(n) };
 
-  constructor(el: ElementRef) { this.element = el; }
+    constructor(el: ElementRef) { this.element = el; }
 
-  ngOnInit() {
-    let options: c3.ChartConfiguration = {
-      bindto: this.element.nativeElement,
-      data: {
-        columns: [[this.data[0] || ' ', this.data[1] || 0]],
-        type: this.type || 'gauge',
-      },
-      gauge: {
-        label: {
-          format: (valor) => {
-            //console.log(valor);
-            return this._f(valor) + ' ' + (this.chartOptions.unidade || '')
-          }
-        },
-        max: this.chartOptions.max || 50,
-        // units: this.chartOptions.unidade || '',
-        width: 20
-      },
-      color: {
-        pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'].reverse(), // the three color levels for the percentage values.
-        threshold: {
-          max: this.chartOptions.max,
-          values: [60, 80, 90, 100]
+    ngOnInit() {
+        let options: c3.ChartConfiguration = {
+            bindto: this.element.nativeElement,
+            data: {
+                columns: [[this.data[0] || ' ', this.data[1] || 0]],
+                type: this.type || 'gauge',
+            },
+            legend: {
+                show: false
+            },
+            gauge: {
+                label: {
+                    format: (valor) => {
+                        //console.log(valor);
+                        return this._f(valor) + ' ' + (this.chartOptions.unidade || '')
+                    }
+                },
+                max: this.chartOptions.max || 50,
+                // units: this.chartOptions.unidade || '',
+                width: 20
+            },
+            color: {
+                pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'].reverse(), // the three color levels for the percentage values.
+                threshold: {
+                    max: this.chartOptions.max,
+                    values: [60, 80, 90, 100]
+                }
+            },
+            size: {
+                height: 100,
+                width: 250
+            }
         }
-      },
-      size: {
-        height: 100,
-        width: 250
-      }
+        this.chart = c3.generate(options);
     }
-    this.chart = c3.generate(options);
-  }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.data && !changes.data.isFirstChange()) {
-      //console.log(changes.data.currentValue);
-      this.chart.load({ columns: [changes.data.currentValue] });
-      //this.chart.resize();
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.data && !changes.data.isFirstChange()) {
+            //console.log(changes.data.currentValue);
+            this.chart.load({ columns: [changes.data.currentValue] });
+            //this.chart.resize();
+        }
     }
-  }
 }
 
 @NgModule({
-  imports: [],
-  declarations: [GaugeDirective],
-  exports: [GaugeDirective]
+    imports: [],
+    declarations: [GaugeDirective],
+    exports: [GaugeDirective]
 })
 export class GaugeDirectiveModule {
 
